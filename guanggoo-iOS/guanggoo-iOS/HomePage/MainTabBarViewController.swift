015c38a41468530c98e20522fc7685b2c0c030b2
tdx-qinken <qinken547@gmail.com>
2018-12-10 09:48:11 +0800

    2.0

---
+++ guanggoo-iOS/guanggoo-iOS/HomePage/MainTabBarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainTabBarViewController.swift
+//  guanggoo-iOS
+//
+//  Created by tdx on 2018/12/7.
+//  Copyright © 2018 tdx. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarViewController: CYLTabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ guanggoo-iOS/guanggoo-iOS/HomePage/PublishButton.swift
@@ -0,0 +1,21 @@
+//
+//  PublishButton.swift
+//  guanggoo-iOS
+//
+//  Created by tdx on 2018/12/7.
+//  Copyright © 2018 tdx. All rights reserved.
+//
+
+import UIKit
+
+class PublishButton: CYLPlusButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
